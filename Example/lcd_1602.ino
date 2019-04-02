b4b03f3641af85328cf52ba31f9f621329248284
DC Back <45322614+dcback@users.noreply.github.com>
2019-04-03 06:31:45 +0900

    Update lcd_1602.ino

--- Example/lcd_1602.ino
+++ Example/lcd_1602.ino
@@ -1,7 +1,7 @@
 /******************************************
-    I2C    Arduino  STM32F103  ESP8266    
-    SDA      4         PB7     D2(GPIO4)     
-    SCL      5         PB6     D1(GPIO5)     
+    I2C    Arduino  STM32F103  ESP8266
+    SDA      4         PB7     D2(GPIO4)
+    SCL      5         PB6     D1(GPIO5)
 *******************************************/
 #define ARDUINO_NANO
 //#define BLUEPILL
@@ -27,14 +27,13 @@ void setup() {
     lcd.begin();        // LCD initialize
     lcd.backlight();    // Backlight on
 
-    lcd.setCursor(3, 0);
-    lcd.print("Hello, world!");
-    lcd.setCursor(2, 1);
-    lcd.print("Ywrobot Arduino!");
-    lcd.setCursor(0, 2);
-    lcd.print("Arduino LCM IIC 2019");
-    lcd.setCursor(2, 3);
-    lcd.print("Power By DC Back!");
+    lcd.print("ABCDEFGHIJKLMNOP");  // ABCDEFGHIJKLMNOP Print
+    lcd.setCursor(0, 1);            // LCD x=0, y=1 point cursor
+    lcd.print("QRSTUVWXYZ012345");  // QRSTUVWXYZ012345 Print
+    lcd.noDisplay();                // LCD Off
+    delay(500);
+    lcd.display();                  // LCD On
+    delay(500);
 }
 
 void loop() {
