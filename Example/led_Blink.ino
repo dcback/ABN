8b1a302d86c42180dc36cd759b5775a301989fad
DC Back <45322614+dcback@users.noreply.github.com>
2019-04-03 00:22:10 +0900

    Update led_Blink.ino

--- Example/led_Blink.ino
+++ Example/led_Blink.ino
@@ -1,4 +1,4 @@
-/*******************************************************************************
+/******************************************************************************************
     #define ARDUINO_NANO
     #define BLUEPILL
     #define NODEMCU
@@ -6,31 +6,27 @@
     #ifdef ARDUINO_NANO
         #define Pins and Funcs()
         #include <abc.h>
-    #endif
-    #ifdef BLUEPILL
+    #elif defined BLUEPILL
         #define Pins and Funcs()
         #include <abc.h>
-    #endif   
-    #ifdef
+    #else //NODEMCU
         #define Pins and Funcs()
         #include <abc.h>
     #endif
-********************************************************************************
+*******************************************************************************************
     Adruino : 1~13, A0~A7
-    STM32   : PA0~PA15, PB0~PB15, PC0~PC15
-    ESP8266 : D0~D10(D0=16, D1=5, D2=4, D3=0, D4=2, D5=14, D6=12, D7=13, D8=15,
-********************************************************************************/
+    STM32   : PA0~PA15, PB0~PB15, PC13~PC15
+    ESP8266 : D0~D10(D0=16, D1=5, D2=4, D3=0, D4=2, D5=14, D6=12, D7=13, D8=15, D9=3, D10=1
+*******************************************************************************************/
 //#define ARDUINO_NANO 
-#define BLUEPILL  
-//#define NODEMCU         
+//#define BLUEPILL  
+#define NODEMCU         
 
 #ifdef ARDUINO_NANO
     #define ledPin 13
-#endif
-#ifdef BLUEPILL
+#elif defined BLUEPILL
     #define ledPin PC13 
-#endif    
-#ifdef NODEMCU
+#else //NODEMCU
     #define ledPin 2
 #endif
 
@@ -41,8 +37,7 @@ void setup() {
     pinMode(ledPin, OUTPUT);
 }
 
-void loop()
-{
+void loop() {
     digitalWrite(ledPin, HIGH);
     delay(1000);
     digitalWrite(ledPin, LOW);
