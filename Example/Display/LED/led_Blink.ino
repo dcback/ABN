01a78c66cbebf906c0bfa6b76e6cb11fd9dbcba1
DC Back <45322614+dcback@users.noreply.github.com>
2019-04-03 14:42:32 +0900

    Update led_Blink.ino

--- Example/Display/LED/led_Blink.ino
+++ Example/Display/LED/led_Blink.ino
@@ -18,9 +18,9 @@
     STM32   : PA0~PA15(0~15), PB0~PB15(16~31), PC13~PC15(32~34)
     ESP8266 : D0~D10(D0=16, D1=5, D2=4, D3=0, D4=2, D5=14, D6=12, D7=13, D8=15, D9=3, D10=1
 *******************************************************************************************/
-//#define ARDUINO_NANO 
+#define ARDUINO_NANO 
 //#define BLUEPILL  
-#define NODEMCU         
+//#define NODEMCU         
 
 #ifdef ARDUINO_NANO
     #define ledPin 13
