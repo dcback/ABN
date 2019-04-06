3f064cd775e9f17cb3cc87923cdf6104d86d56e0
DC Back <45322614+dcback@users.noreply.github.com>
2019-04-07 08:42:26 +0900

    Create mySerial_RX_STR_abn.ino

---
+++ Example/SoftwareSerial/TRX_str/mySerial_RX_STR_abn.ino
@@ -0,0 +1,39 @@
+/********************************************
+    ARDUINO  <-> NODEMCU  ok
+    BLUEPILL <-> NODEMCU  Data Break
+    ARDUINO  <-> BLUEPILL Data Break
+/*******************************************/
+//#define ARDUINO_NANO
+//#define BLUEPILL
+#define NODEMCU
+
+#ifdef ARDUINO_NANO
+    #include "SoftwareSerial.h"
+    #define GPIO_SERIAL_RX    A2
+    #define GPIO_SERIAL_TX    A3
+    SoftwareSerial mySerial(GPIO_SERIAL_RX, GPIO_SERIAL_TX);
+#elif defined BLUEPILL
+    #include "SoftSerialSTM32.h"
+    #define GPIO_SERIAL_RX    PA2
+    #define GPIO_SERIAL_TX    PA3
+    SoftSerialSTM32 mySerial(GPIO_SERIAL_RX, GPIO_SERIAL_TX);
+#else //NODEMCU
+    #include "SoftwareSerial.h"
+    #define GPIO_SERIAL_RX    D2
+    #define GPIO_SERIAL_TX    D3
+    SoftwareSerial mySerial(GPIO_SERIAL_RX, GPIO_SERIAL_TX);
+#endif
+
+void setup() {
+    Serial.begin(9600);
+    mySerial.begin(9600);
+}
+
+void loop() {
+    while ( mySerial.available() > 0 ) {
+        char serIn = mySerial.read();   // read Serial byte unit
+        Serial.print(serIn);            // print the character just read
+    }
+    Serial.println();          
+    delay(1000);
+}
