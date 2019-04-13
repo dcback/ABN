bafcb1e34a42930ec479fa8e225caed1e82e3ed2
DC Back <45322614+dcback@users.noreply.github.com>
2019-04-13 14:13:05 +0900

    Update pin_Check.ino

--- Example/Utility/pin_Check.ino
+++ Example/Utility/pin_Check.ino
@@ -1,23 +1,15 @@
-/******************************************************************************************
-    #define ARDUINO_NANO
-    #define BLUEPILL
-    #define NODEMCU
-
-    #ifdef ARDUINO_NANO
-        #define Pins and Funcs()
-        #include <aaa.h>
-    #elif defined BLUEPILL
-        #define Pins and Funcs()
-        #include <bbb.h>
-    #else //NODEMCU
-        #define Pins and Funcs()
-        #include <ccc.h>
-    #endif
-*******************************************************************************************
-    Adruino : 1~13, A0~A7(14~21)
-    STM32   : PA0~PA15(0~15), PB0~PB15(16~31), PC13~PC15(32~34)
-    ESP8266 : D0~D10(D0=16, D1=5, D2=4, D3=0, D4=2, D5=14, D6=12, D7=13, D8=15, D9=3, D10=1
-*******************************************************************************************/
+/************************************************************************************
+     Arduino: D0 D1 D2 D3 D4 D5 D6 D7 D8 D9 D10 D11 D12 D13 A0 A1 A2 A3 A4 A5 A6 A7
+        GPIO: 0  1  2  3  4  5  6  7  8  9  10  11  12  13  14 15 16 17 18 19 20 21
+    Bluepill: PA0 PA1 PA2 PA3 PA4 PA5 PA6 PA7 PA8 PA9 PA10 PA11 PA12 PA13 PA14 PA15 
+        GPIO: 0   1   2   3   4   5   6   7   8   9   10   11   12   13   14   15 
+              PB0 PB1 PB2 PB3 PB4 PB5 PB6 PB7 PB8 PB9 PB10 PB11 PB12 PB13 PB14 PB15
+              16  17  18  19  20  21  22  23  24  25  26   27   28   29   30   31
+              PC13 PC14 PC15
+              32   33   34
+    ESP8266 : D0 D1 D2 D3 D4 D5 D6 D7 D8 D9 D10
+        GPIO: 16 5  4  0  2  14 12 13 15 3  1 
+*************************************************************************************/
 #define ARDUINO_NANO
 //#define BLUEPILL
 //#define NODEMCU
