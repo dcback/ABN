19cf158a27132bdbf6973fc9c28574224e91b35c
DC Back <45322614+dcback@users.noreply.github.com>
2019-09-01 18:44:11 +0900

    Create gpio_control04.ino

---
+++ IoT/Basic/gpio_control04.ino
@@ -0,0 +1,30 @@
+#define NANO        // NANO 보드인 경우 define
+//#define BLUEPILL  // BLUEPILL 보드인 경우 define
+//#define NODEMCU   // NODEMCU 보드인 경우 define
+
+#ifdef NANO             // NANO가 정의되면
+#define buzzPin 5       // buzzPin을 A2로 하고 
+#endif                  // #ifdef 사용 후 반드시 #endif
+
+#ifdef BLUEPILL         // BLUEPILL이 정의되면
+#define buzzPin PB8     // buzzPin을 PB13로 하고
+#endif                  // #ifdef 사용 후 반드시 #endif
+
+#ifdef NODEMCU          // BLUEPILL이 정의되면
+#define buzzPin D3      // buzzPin을 D3로 하고
+#endif                  // #ifdef 사용 후 반드시 #endif
+
+int delayTime = 500;    // 지연시간(0.5ms) 변수 할당
+
+void setup()
+{
+    pinMode(buzzPin, OUTPUT);
+}
+
+void loop()
+{
+    digitalWrite(buzzPin, HIGH);  // Buzz On HIGH
+    delayMicroseconds(delayTime); // delayTime 지연
+    digitalWrite(buzzPin, LOW);   // Buzz Off LOW
+    delayMicroseconds(delayTime); // delayTime 지연
+}
